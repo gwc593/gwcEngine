52db8c6699fb459a132b54038beeaf814e445659
Guy Collins <gwc593@gmail.com>
2020-12-22 20:57:10 +0000

    Commit before Ray Branch

--- gwcEngine/gwcEngine/src/gwcEngine/Renderer/Renderer.h
+++ gwcEngine/gwcEngine/src/gwcEngine/Renderer/Renderer.h
@@ -24,6 +24,8 @@ namespace gwcEngine
 		static void Submit(const Ref<VertexArray>& vertexArray, const Ref<Shader>& shader, const glm::mat4& transform = glm::mat4(1.0f));
 		static void Submit(const Ref<VertexArray>& vertexArray, const Ref<Material>& material, const glm::mat4& transform = glm::mat4(1.0f));
 
+
+
 		inline static RendererAPI::API GetAPI() { return RendererAPI::GetAPI(); }
 
 	private:
